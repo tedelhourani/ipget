86b4fdcb6e7eae47082b01dcf7b895a25fcdf10e
Ted Elhourani <theodore.elhourani@gmail.com>
2014-08-15 13:33:51 -0700

    GPL license appended

--- ipget.py
+++ ipget.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+
+#   ip_get daemon
+#   dependencies: python-daemon
 #
-# ip_get daemon
-# prerequisites: python-daemon
-#
+#   Copyright (C) 2014  Theodore Elhourani
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 try:
     import subprocess
     import imaplib
@@ -18,13 +30,13 @@ except ImportError as error:
 
 class IpGet():
     def __init__(self, address, password):
-        self.CHECK_FREQUENCY = 60
+        self.check_frequency = 60
         self.getipcommand = 'curl -s http://checkip.dyndns.org/ | cut -d \' \' -f 6 | sed s/\"body\|html\|<\|>\|\/\"//g'
         self.IMAP = 'imap.gmail.com'
         self.SMTP = 'smtp.gmail.com'
         # using email address
-        self.USERNAME = address
-        self.PASSWORD = password
+        self.username = address
+        self.password = password
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
@@ -34,8 +46,8 @@ class IpGet():
     def open_imap_connection(self, verbose=False):
         if verbose: print 'Connecting to', self.IMAP
         connection = imaplib.IMAP4_SSL(self.IMAP)
-        if verbose: print 'Logging in as', self.USERNAME
-        connection.login(self.USERNAME, self.PASSWORD)
+        if verbose: print 'Logging in as', self.username
+        connection.login(self.USERNAME, self.password)
         return connection
 
     def respond(self, verbose=False):
@@ -68,7 +80,7 @@ class IpGet():
                     typ, response = c.expunge()
             finally:
                 c.logout()
-            time.sleep( self.CHECK_FREQUENCY )
+            time.sleep( self.check_frequency )
 
 if __name__ == '__main__':
     address = raw_input('email address:')
